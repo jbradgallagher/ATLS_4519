401fb4cba3b3aec210d9f515d41fd3e6cfd2aaea
Brad Gallagher <jbradgallagher@gmail.com>
2022-01-31 16:34:18 -0700

    small changes to markov_simple.py to make it write output to a series of file over a number of runs

--- 01312022/markov_simple.py
+++ 01312022/markov_simple.py
@@ -5,17 +5,21 @@ import getopt
 
 
 
-def printMarkov(inFile,levels,numSentences,numTries):
+def printMarkov(inFile,levels,numSentences,numTries,numRuns):
 	with open(inFile) as f:
 		text = f.read()
 
 	text_model = markovify.Text(text, state_size=levels)
 
-	for i in range(numSentences):
-		print(text_model.make_sentence(tries=numTries))
+	for i in range(numRuns):
+		outfile = "markov_" + "%04d" % i + ".txt"
+		outfh = open(outfile, "w")
+		for k in range(numSentences):
+			outfh.write(text_model.make_sentence(tries=numTries) + "\n")
+		outfh.close()
 
 def printUsage():
-	print("python3 markov_simple.py -f <corpus> -n <levels> -N <num sentences> -t <num tries>")
+	print("python3 markov_simple.py -f <corpus> -n <levels> -N <num sentences> -t <num tries> -r <num runs>")
 	sys.exit()
 
 def main():
@@ -25,12 +29,13 @@ def main():
 	levels = 1
 	numSentences = 3
 	numTries = 100
+	numRuns = 1
 
 	if(len(sys.argv) == 1):
 		printUsage()
 	else:
 		try:
-			opts, args = getopt.getopt(sys.argv[1:], 'f:n:N:t:')
+			opts, args = getopt.getopt(sys.argv[1:], 'f:n:N:t:r:')
 			for o, a in opts:
 				if o == '-f':
 					inFile = a
@@ -40,10 +45,13 @@ def main():
 					numSentences = int(a)
 				if o == '-t':
 					numTries = int(a)
+				if o == '-r':
+					numRuns = int(a)
+
 			
 
 			
-			printMarkov(inFile,levels,numSentences,numTries)
+			printMarkov(inFile,levels,numSentences,numTries,numRuns)
 		
 					
 		except getopt.GetoptError as err:
